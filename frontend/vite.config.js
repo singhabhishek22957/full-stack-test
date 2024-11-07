ddd9ba26789470cfcf590f4af051f23d4feaa500
singhabhishek22957 <singhabhishek22957@gmai.com>
2024-11-08 05:03:59 +0530

    Hello link proxyes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api':'http://localhost:5000',
+      // '/api':'http://localhost:5000',
+      '/api':'https://abhishek-website-i74a.onrender.com',
     },
   },
   plugins: [react()],
